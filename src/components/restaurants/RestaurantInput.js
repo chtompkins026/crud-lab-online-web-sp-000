938f729ed58184b69e88895c1daa5bce4349dcb2
Christopher Tompkins <chtompkins026@gmail.com>
2019-03-11 15:47:45 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import ReviewsContainer from '../../containers/ReviewsContainer'; 
 
-class RestaurantInput extends Component {
+class Restaurant extends Component {
+
+  handleOnClick() {
+    this.props.deleteRestaurant(this.props.restaurant.id);
+  }
+
   render() {
+    const { restaurant } = this.props;
+
     return (
       <div>
-        Restaurant Input
+        <li>
+          {restaurant.text}
+          <button onClick={() => this.handleOnClick()}> X </button>
+          <ReviewsContainer restaurant={restaurant}/>
+        </li>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+export default Restaurant;
